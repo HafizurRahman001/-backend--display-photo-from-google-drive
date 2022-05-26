1889a5412d26f10515f4391553740791186aadb8
Hafizur <hafizurrahmanbu@gmail.com>
2022-05-27 02:53:04 +0600

    added env file

--- index.js
+++ index.js
@@ -17,7 +17,7 @@ app.use(express.json());
 // ${process.env.DB_USER}: ${process.env.DB_PASS}
 //DB_USER=mydbuser1      DB_PASS=Q0AlzP3yLxbR7NDs
 
-const uri = "mongodb+srv://mydbuser1:Q0AlzP3yLxbR7NDs@cluster0.h7sw1.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.h7sw1.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
 
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
 
